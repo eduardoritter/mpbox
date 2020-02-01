5b3785a0278213a846e331736649669a27f88b3d
Eduardo Ritter <eduardoritter@gmail.com>
2020-02-01 18:18:04 -0300

    unit

--- mpbox/__init__.py
+++ mpbox/__init__.py
@@ -7,52 +7,43 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_babel import Babel
 
-
+app = Flask(__name__)
 db = SQLAlchemy()
 login_manager = LoginManager()
+babel = Babel(app)
 
+app.config.from_mapping(
+    SECRET_KEY="secret",
+    SQLALCHEMY_DATABASE_URI="sqlite:///mpbox.sqlite",
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    BABEL_DEFAULT_LOCALE="pt_BR",
+    BABEL_DEFAULT_TIMEZONE="Brasilia"
+)
 
-def create_app():
-    """Create and configure an instance of the Flask application."""
-    app = Flask(__name__)
-    
-    babel = Babel(app)
+@app.route("/hi")
+def hello():
+    return "Hi, I'm up!"
 
-    app.config.from_mapping(        
-        SECRET_KEY="secret",        
-        SQLALCHEMY_DATABASE_URI="sqlite:///mpbox.sqlite",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        BABEL_DEFAULT_LOCALE="pt_BR",
-        BABEL_DEFAULT_TIMEZONE="Brasilia"
-    )
+db.init_app(app)
+Migrate(app, db)
 
-    @app.route("/hi")
-    def hello():
-        return "Hi, I'm up!"
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    db.init_app(app)
-    Migrate(app, db)
+from mpbox import patient
+app.register_blueprint(patient.bp)
 
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+from mpbox import plan
+app.register_blueprint(plan.bp)
 
-    from mpbox import patient   
-    app.register_blueprint(patient.bp)
+from mpbox import visit
+app.register_blueprint(visit.bp)
 
-    from mpbox import plan
-    app.register_blueprint(plan.bp)
+from mpbox import auth
+app.register_blueprint(auth.bp)
 
-    from mpbox import visit
-    app.register_blueprint(visit.bp)
-
-    from mpbox import auth
-    app.register_blueprint(auth.bp)
-
-    
-    # make url_for('index') == url_for('patient.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule("/", endpoint="patient.index")
-
-    return app
+# make url_for('index') == url_for('patient.index')
+# in another app, you might define a separate main index here with
+# app.route, while giving the blog blueprint a url_prefix, but for
+# the tutorial the blog will be the main index
+app.add_url_rule("/", endpoint="patient.index")
---
+++ mpbox/test_core.py
@@ -0,0 +1,17 @@
+from mpbox import app
+
+# Importamos a biblioteca de testes
+import unittest
+
+class MPBoxTest(unittest.TestCase):
+
+    def setUp(self):
+        appp = app.test_client()
+        self.response = appp.get('/hi')
+    
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_content(self):
+        response_str = self.response.data.decode('utf-8')
+        self.assertIn("Hi, I'm up!", str(response_str))
