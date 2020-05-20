54a922d041d5968f1f95c76a4685a90cd1b8ad0d
Eduardo Ritter <eduardoritter@gmail.com>
2020-05-19 23:53:24 -0300

    dash

--- mpbox/dashboard.py
+++ mpbox/dashboard.py
@@ -1,14 +1,15 @@
-
 from flask import Blueprint, render_template, request, redirect, request, url_for, flash
 from flask_login import login_required
-from model import Plan
+
+from mpbox.model import Plan
+from mpbox.config import BASE_URL_PREFIX
 
 
 bp = Blueprint('dashboard', __name__, url_prefix=BASE_URL_PREFIX + 'dashboard')
 
 
-@bp.route("/")
+@bp.route('/')
 @login_required
-def dash():
+def dashboard():
     last_plans = Plan.query.order_by(Plan.created.desc()).limit(5)    
     return render_template('dashboard.html', last_plans=last_plans)
