e510cb887f9dd482ed8aeb11e167362435cc0f44
Eduardo Ritter <eduardoritter@gmail.com>
2020-05-27 23:46:50 -0300

    dashboard update

--- mpbox/dashboard.py
+++ mpbox/dashboard.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from flask import Blueprint, render_template, request, redirect, request, url_for, flash
 from flask_login import login_required
 
-from mpbox.model import Plan
+from mpbox.model import Plan, Visit
 from mpbox.config import BASE_URL_PREFIX
 
 
@@ -14,5 +15,18 @@ def dashboard():
     last_plans = Plan.query.order_by(Plan.created.desc()).limit(5)    
     return render_template('dashboard.html', last_plans=last_plans)
 
-def expired_plans
+def expired_plans():
     return
+
+@bp.route('/search')
+@login_required
+def search():
+
+    try:
+        visit_date = datetime.strptime(request.args.get('visit_date'), '%d/%m/%Y')
+        visits = Visit.query.filter(Visit.date == visit_date.date())
+    except Exception as error:
+        flash(error)
+        return render_template('dashboard.html')
+    
+    return render_template('dashboard.html', visits=visits)
